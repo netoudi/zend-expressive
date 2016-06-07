b457d2b81edc5e46fb3cb8b58a4570196e5a6b20
netoudi <netoudi@rocketmail.com>
2016-06-07 18:22:29 -0300

    Criando namespace da aplicação.

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -4,11 +4,11 @@ return [
     'dependencies' => [
         'invokables' => [
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\AuraRouter::class,
-            App\Action\PingAction::class => App\Action\PingAction::class,
+            CodeEmailMKT\Action\PingAction::class => CodeEmailMKT\Action\PingAction::class,
         ],
         'factories' => [
-            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
-            App\Action\TestPageAction::class => App\Action\TestPageFactory::class,
+            CodeEmailMKT\Action\HomePageAction::class => CodeEmailMKT\Action\HomePageFactory::class,
+            CodeEmailMKT\Action\TestPageAction::class => CodeEmailMKT\Action\TestPageFactory::class,
         ],
     ],
 
@@ -16,19 +16,19 @@ return [
         [
             'name' => 'home',
             'path' => '/',
-            'middleware' => App\Action\HomePageAction::class,
+            'middleware' => CodeEmailMKT\Action\HomePageAction::class,
             'allowed_methods' => ['GET'],
         ],
         [
             'name' => 'api.ping',
             'path' => '/api/ping',
-            'middleware' => App\Action\PingAction::class,
+            'middleware' => CodeEmailMKT\Action\PingAction::class,
             'allowed_methods' => ['GET'],
         ],
         [
             'name' => 'teste',
             'path' => '/teste',
-            'middleware' => App\Action\TestPageAction::class,
+            'middleware' => CodeEmailMKT\Action\TestPageAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
