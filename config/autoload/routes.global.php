42f8bbdc88184ac80060d7de1d92e3830e787344
netoudi <netoudi@rocketmail.com>
2016-06-07 11:22:37 -0300

    Primeira Aplicação.

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -8,6 +8,7 @@ return [
         ],
         'factories' => [
             App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
+            App\Action\TestPageAction::class => App\Action\TestPageFactory::class,
         ],
     ],
 
@@ -22,6 +23,12 @@ return [
             'name' => 'api.ping',
             'path' => '/api/ping',
             'middleware' => App\Action\PingAction::class,
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'teste',
+            'path' => '/teste',
+            'middleware' => App\Action\TestPageAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
---
+++ src/App/Action/TestPageAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Template;
+
+class TestPageAction
+{
+    private $template;
+
+    public function __construct(Template\TemplateRendererInterface $template = null)
+    {
+        $this->template = $template;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        return new HtmlResponse($this->template->render('app::test-page', ['data' => 'Minha primeira aplicação!']));
+    }
+}
