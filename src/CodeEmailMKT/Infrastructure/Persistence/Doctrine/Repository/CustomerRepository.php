4c766acbc92a5c67698ab827b7d163c87f37d83c
netoudi <netoudi@rocketmail.com>
2016-06-15 17:56:19 -0300

    Criando CustomerRepository.

---
+++ src/CodeEmailMKT/Infrastructure/Persistence/Doctrine/Repository/CustomerRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CodeEmailMKT\Infrastructure\Persistence\Doctrine\Repository;
+
+use CodeEmailMKT\Domain\Persistence\CustomerRepositoryInterface;
+use Doctrine\ORM\EntityRepository;
+
+class CustomerRepository extends EntityRepository implements CustomerRepositoryInterface
+{
+
+    public function create($entity)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update($entity)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function remove($entity)
+    {
+        // TODO: Implement remove() method.
+    }
+
+    public function find($id)
+    {
+        // TODO: Implement remove() method.
+    }
+
+    public function findAll()
+    {
+        // TODO: Implement remove() method.
+    }
+}
