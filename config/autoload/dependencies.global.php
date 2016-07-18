86d31888df7df165ce0537073ca26eeec72aa271
netoudi <netoudi@rocketmail.com>
2016-07-18 11:43:47 -0300

    Pegando twig enviroment.

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -29,7 +29,8 @@ return [
             FlashMessageInterface::class => FlashMessageFactory::class,
         ],
         'aliases' => [
-            'configuration' => 'config', //Doctrine needs a service called Configuration
+            'Configuration' => 'config', //Doctrine needs a service called Configuration
+            'Config' => 'config', //Doctrine needs a service called Configuration
         ],
     ],
 ];
