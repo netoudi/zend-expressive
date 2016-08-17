fa25a6669ba0f51f0c6144921a0768236de9c708
netoudi <netoudi@rocketmail.com>
2016-08-17 16:32:03 -0300

    Criando CustomerInputFilter.

---
+++ src/CodeEmailMKT/Application/InputFilter/CustomerInputFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CodeEmailMKT\Application\InputFilter;
+
+use Zend\Filter\StringTrim;
+use Zend\Filter\StripTags;
+use Zend\Validator\EmailAddress;
+use Zend\Validator\NotEmpty;
+
+class CustomerInputFilter
+{
+    /**
+     * CustomerInputFilter constructor.
+     */
+    public function __construct()
+    {
+        $this->add([
+            'name' => 'name',
+            'required' => false,
+            'filters' => [
+                ['name' => StringTrim::class],
+                ['name' => StripTags::class],
+            ],
+        ]);
+
+        $this->add([
+            'name' => 'email',
+            'required' => true,
+            'filters' => [
+                ['name' => StringTrim::class],
+            ],
+            'validators' => [
+                ['name' => NotEmpty::class],
+                ['name' => EmailAddress::class],
+            ],
+        ]);
+    }
+}
